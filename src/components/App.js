1d5a2e11cfb81bdeb1d2146fc306cfc927fb5305
kronos <dua_bhavya@outlook.com>
2023-10-07 01:36:18 -0400

    Create function to update state upon creation of new team

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
 import '../App.css';
 import Header from './Header';
 
 function App() {
+
+  const [teams, setTeams] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/teams")
+      .then(r => r.json())
+      .then(setTeams)
+  }, [])
+
+  function addNewTeam(newTeam) {
+    setTeams([
+      ...teams,
+      newTeam
+    ])
+  }
+
+
   return (
     <div className="App">
       <Header />
