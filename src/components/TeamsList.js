8b5db2d6d580f44d4e1696dc014cd6c098a6cf89
kronos <dua_bhavya@outlook.com>
2023-10-07 19:44:54 -0400

    Display cards in TeamList

--- src/components/TeamsList.js
+++ src/components/TeamsList.js
@@ -1,7 +1,17 @@
 import React from "react";
+import TeamCard from "./TeamCard";
 
 function TeamsList({ teams }) {
 
+    const toDisplay = teams.map(team => (
+        <TeamCard
+        id={team.id}
+        teamName={team.team_name}
+        teamLogo={team.team_logo}
+        wins={team.wins}
+        losses={team.losses}
+        />
+    ))
     
 
     return (
