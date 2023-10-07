f12753f0968a0fc3c5aac1d085a552161cd934b3
kronos <dua_bhavya@outlook.com>
2023-10-07 01:06:26 -0400

    Create form for new team

---
+++ src/components/NewTeamForm.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function NewTeamForm() {
+
+    const [formData, setFormData] = useState({teamName: "", teamLogo: "", wins: 0, losses: 0})
+
+
+    return (
+        <form className="ui form" onSubmit={handleSubmit}>
+            <div className="field">
+            <label htmlFor="teamName">Team Name </label>
+            <input type="text" id="teamName" name="teamName" value={formData.teamName} onChange={handleFormData} />
+            </div>
+            <div className="field">
+            <label htmlFor="teamLogo">Team Logo </label>
+            <input type="text" id="teamLogo" name="teamLogo" value={formData.teamLogo} onChange={handleFormData} />
+            </div>
+            <div className="field">
+            <label htmlFor="wins">Wins </label>
+            <input type="number" min="0" id="wins" name="wins" value={formData.wins} onChange={handleFormData} />
+            </div>
+            <div className="field">
+            <label htmlFor="losses">Losses </label>
+            <input type="number" min="0" id="losses" name="losses" value={formData.losses} onChange={handleFormData} />
+            </div>
+            <button className="ui button" type="submit">Submit</button>
+        </form>
+    )
+}
