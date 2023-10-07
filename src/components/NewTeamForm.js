bb58ad0fec2b77309634d8cd788896afa43165ba
kronos <dua_bhavya@outlook.com>
2023-10-07 01:22:01 -0400

    Create function to handle submission of form

--- src/components/NewTeamForm.js
+++ src/components/NewTeamForm.js
@@ -2,7 +2,22 @@ import React, { useState } from "react";
 
 function NewTeamForm() {
 
-    const [formData, setFormData] = useState({teamName: "", teamLogo: "", wins: 0, losses: 0})
+    const [formData, setFormData] = useState({team_name: "", team_logo: "", wins: 0, losses: 0})
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        e.target.reset()
+
+        fetch("http://localhost:9292/teams", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData)
+    })
+        .then(r => r.json())
+        .then(addNewTeam)
+    }
 
     function handleFormData(e) {
         setFormData({
@@ -11,16 +26,15 @@ function NewTeamForm() {
         })
     }
 
-
     return (
         <form className="ui form" onSubmit={handleSubmit}>
             <div className="field">
-            <label htmlFor="teamName">Team Name </label>
-            <input type="text" id="teamName" name="teamName" value={formData.teamName} onChange={handleFormData} />
+            <label htmlFor="team_name">Team Name </label>
+            <input type="text" id="team_name" name="team_name" value={formData.team_name} onChange={handleFormData} />
             </div>
             <div className="field">
-            <label htmlFor="teamLogo">Team Logo </label>
-            <input type="text" id="teamLogo" name="teamLogo" value={formData.teamLogo} onChange={handleFormData} />
+            <label htmlFor="team_logo">Team Logo </label>
+            <input type="text" id="team_logo" name="team_logo" value={formData.team_logo} onChange={handleFormData} />
             </div>
             <div className="field">
             <label htmlFor="wins">Wins </label>
