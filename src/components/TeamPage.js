279aa9aea0db36211219fa43a2362a24c4c362e5
kronos <dua_bhavya@outlook.com>
2023-10-10 14:16:20 -0400

    Create EditPlayerForm

--- src/components/TeamPage.js
+++ src/components/TeamPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { Link, useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 function TeamPage() {
 
@@ -17,6 +17,7 @@ function TeamPage() {
             <div className="content">
                 <h4 className="header">{player.first_name + " " + player.last_name}</h4>
                 <h3 className="age">{player.age}</h3>
+                <Link to={`/players/${player.id}`}>Edit Player</Link>
             </div>
         </div>
     ))
