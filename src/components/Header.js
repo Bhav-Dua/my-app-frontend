674af39545c885270b08e8af19c8ef749f143128
kronos <dua_bhavya@outlook.com>
2023-10-10 14:56:51 -0400

    Make header logo link to homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,12 @@ import React from "react";
 import logo from "../nba_logo.png"
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
-
 function Header() {
     return (
         <div className="header">
-            <img className="logo" src={logo} alt="NBA" />
+            <Link to="/">
+                <img className="logo" src={logo} alt="NBA" />
+            </Link>
             <Link to="/newTeam">New Team</Link>
         </div>
     )
